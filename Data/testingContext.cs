d8723874e7fa127d2d4a5faaa054452c8439214c
Shujat Ali <the.shujat@gmail.com>
2022-11-23 10:23:18 +0100

    Add project files.

---
+++ Data/testingContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using testing.Data.Entities;
+
+namespace testing.Data
+{
+    public class testingContext : DbContext
+    {
+        public testingContext (DbContextOptions<testingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<testing.Data.Entities.Cat> Cat { get; set; } = default!;
+
+        public DbSet<testing.Data.Entities.Person> Person { get; set; } = default!;
+    }
+}
