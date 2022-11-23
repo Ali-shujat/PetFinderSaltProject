e963780e9bee5855843dc93a7e7924366fe35efb
Shujat Ali <the.shujat@gmail.com>
2022-11-23 12:12:49 +0100

    add new Nav

--- App/src/App.js
+++ App/src/App.js
@@ -7,17 +7,54 @@ import Navbar from './pages/Navbar';
 import Sighter from './pages/Sighter';
 import CustomMap from './components/CustomMap';
 import { Container } from '@material-ui/core';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  // Temporary test values
+  const kattTheme = createTheme({
+    palette: {
+      primary: {
+        light: '#e2a2b4',
+        main: '#c44569',
+        dark: '#622335',
+        contrastText: '#fff5e2',
+
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+      },
+    },
+  });
+
   return (
     <>
-      <Navbar />
-      <div className="App"> 
-        {/* <FileUpload /> */}
-        <Sighter/>
-        {/* <Wanting/> */}
-      </div>     
-      <BottomNav />
+      <ThemeProvider theme={kattTheme}>
+
+        <Container maxWidth="lg">
+          <Navbar />
+
+          <main id="ContentArea">
+            <Routes>
+              <Route path="" element={<Wanting />} />
+              <Route path="/" element={<Wanting />} />
+              <Route path="/reportsighting" element={<FileUpload />} />
+              <Route path="/map" element={<CustomMap />} />
+              <Route path="/login" element={<Sighter />} />
+              <Route path="/about" element={<Wanting />} />
+            </Routes>
+          </main>
+
+          {/* <footer>
+            Footer
+          </footer> */}
+        </Container>
+        <BottomNav></BottomNav>
+      </ThemeProvider>
     </>
   );
 }
