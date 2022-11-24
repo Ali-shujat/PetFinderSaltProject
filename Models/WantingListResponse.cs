862e145cfa7ccc1c1417066d62cd9458f75ed594
Guido <108191578+Guibrick@users.noreply.github.com>
2022-11-24 17:26:11 +0100

    Fixing WantingListResponse.cs

--- Models/WantingListResponse.cs
+++ Models/WantingListResponse.cs
@@ -6,11 +6,8 @@ namespace PetFinderApi.Models;
 
 public class WantingListResponse
 {
-<<<<<<< HEAD
     //public List<Wanting>
-=======
-   // public List<Wanting
->>>>>>> d8a8d8c1989eeb0c995a2d506fb8f77fe8fcc731
+
     //TODO: check håkans!
     [Required]
     public string CatName { get; set; }
