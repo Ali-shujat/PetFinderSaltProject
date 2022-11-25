47137f67c4aa265f338c918915cead17ecb060ed
tamtaram <107140831+tamtaram@users.noreply.github.com>
2022-11-25 10:37:30 +0100

    Reapply connectionstring + modify create endpoing

--- Controllers/WantingController.cs
+++ Controllers/WantingController.cs
@@ -11,17 +11,17 @@ public class WantingController : ControllerBase
 {
     private Mapper mapper = new Mapper();
     private readonly PetFinderContext _context;
-    private IWantingService _service;
+    private IWantingService _dbservice;
     public WantingController(IWantingService service, PetFinderContext context)
     {
-        _service = service;
+        _dbservice = service;
         _context = context;
     }
 
     [HttpGet]
     public async Task<ActionResult<WantingListResponse>> GetAllWanting()
     {
-        var wantings = await _service.GetAll();
+        var wantings = await _dbservice.GetAll();
         var WantingsToList = mapper.getAll(wantings);
         return Ok(WantingsToList);
     }
@@ -29,7 +29,7 @@ public class WantingController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<Wanting>> GetOneWanting(int id)
     {
-        var wanting = await _service.GetOne(id);
+        var wanting = await _dbservice.GetOne(id);
         if (wanting == null) return NotFound();
         var response = mapper.makeOne(wanting);
         return Ok(response);
@@ -37,27 +37,9 @@ public class WantingController : ControllerBase
 
     [HttpPost]
     public async Task<ActionResult<Wanting>> Create(WantingRequest request)
-     {
-    //     var newCat = new Cat()
-    //     {
-    //         Name = request.CatName,
-    //         Owner = new Person()
-    //         {
-    //             FirstName = request.OwnerName
-    //         }
-    //     };
-    //     var wanting = new Wanting
-    //     {
-    //         Cat = newCat,
-    //         EventInfo = request.EventInfo,
-    //         Latitud = request.Position[0],
-    //         Longitud = request.Position[1],
-    //     };
-
-    //     _context.Wanting.Add(wanting);
-    //     await _context.SaveChangesAsync();
-    var wanting = mapper.WantingReqToWanting(request);
-    var wantingCreated = await _service.Create(wanting);
-        return Ok(mapper.makeOne(wantingCreated));
+    {
+        var wanting = mapper.WantingReqToWanting(request);
+        await _dbservice.Create(wanting);
+        return CreatedAtAction("GetOneWanting", new { id = wanting.Id }, mapper.makeOne(wanting));
     }
 }
