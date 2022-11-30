fe36b712c67d362518a7719c93e7ba9f0facd50b
tamtaram <107140831+tamtaram@users.noreply.github.com>
2022-11-30 10:10:57 +0100

    Add working endpoint takes position as string instead of double[]

---
+++ Migrations/20221129141255_imagecolumnInSighting.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace testing.Migrations
+{
+    /// <inheritdoc />
+    public partial class imagecolumnInSighting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "imageFileName",
+                table: "Sighting",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "imageFileName",
+                table: "Sighting");
+        }
+    }
+}
--- Models/WantingRequest.cs
+++ Models/WantingRequest.cs
@@ -6,7 +6,10 @@ public class WantingRequest
     public string? OwnerName { get; set; }
     public string? Email { get; set; }
     public string? CatName { get; set; }
-    public double[]? Position { get; set; }
+    public string? Position { get; set; }
     public string? Description { get; set; }
     public IFormFile image { get; set; }
+    
+    public string? User { get; set; }
+    
 }
